5af7957e5241a73d751abd0ec2a0adff2e942ad6
gummygyaru <vponpobusiness@gmail.com>
2025-09-08 16:52:30 -0700

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -26,7 +26,7 @@ charadex.site = {
 /* ==================================================================== */
 charadex.sheet = {
 
-  id: "1GwgfLizD3HQCieGia6di-TfU4E3EipT9Jb0BDZQwNak",
+  id: "1i8nFkXoGJT9Z0vjveeLbkPNbV-6ZdQlnG1uZ64OR5bM",
 
   pages: {
     masterlist:    "masterlist",
