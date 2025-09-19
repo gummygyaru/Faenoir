102b0c48a2acb93984d5f5316399d41dcbacfcfd
gummygyaru <vponpobusiness@gmail.com>
2025-09-19 16:11:42 -0700

    Update config.js

--- styles/js/config.js
+++ styles/js/config.js
@@ -16,7 +16,7 @@ let charadex = {};
 /* ==================================================================== */
 charadex.site = {
   title: "Charadex",
-  url: "https://charadex-team.github.io/charadex-v1.0/",
+  url: "https://gummygyaru.github.io/Faenoir/",
   description: `A tool for organizing small ARPGs and species.`
 }
 
