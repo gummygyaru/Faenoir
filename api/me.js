299eadb8869d2c01ad38030e8c6c5d9937d8b691
gummygyaru <vponpobusiness@gmail.com>
2025-11-04 16:07:29 -0800

    Update me.js

--- api/me.js
+++ api/me.js
@@ -1,1 +1,10 @@
+import cookie from "cookie";
+
+export default function handler(req, res) {
+  const cookies = cookie.parse(req.headers.cookie || "");
+  if (!cookies.toyhou_user) return res.status(401).json({ error: "Not logged in" });
+
+  const user = JSON.parse(cookies.toyhou_user);
+  res.status(200).json({ user });
+}
 
