ca9dbe34b9ed0aaed83d487c84c322c6e3557039
gummygyaru <vponpobusiness@gmail.com>
2025-11-04 16:06:59 -0800

    Update toyhou.js

--- api/auth/toyhou.js
+++ api/auth/toyhou.js
@@ -1,1 +1,20 @@
+import { URLSearchParams } from "url";
+
+export default function handler(req, res) {
+  const {
+    TOYHOU_CLIENT_ID,
+    BACKEND_BASE_URL
+  } = process.env;
+
+  const redirectUri = `${BACKEND_BASE_URL}/api/auth/callback`;
+
+  const params = new URLSearchParams({
+    client_id: TOYHOU_CLIENT_ID,
+    redirect_uri: redirectUri,
+    response_type: "code",
+    state: "faenoir-optional-csrf"
+  }).toString();
+
+  res.redirect(`https://toyhou.se/~oauth/authorize?${params}`);
+}
 
